fc1af38b1bad1f233d5210f4938adb8ead57aa10
Dan Pelis <dppelis@gmail.com>
2020-05-08 23:41:13 -0400

    Added psnr calculation Also added prompts for user to input quant step amount and desired wavelet Cleaned up unnecessary lines and added comments

--- subbandEncoder.m
+++ subbandEncoder.m
@@ -1,15 +1,24 @@
-%If png must read and show as [img, cmap]
+%Closes all open figures as to not clutter user's screen after multiple
+%runs
 close all;
+
+% Reads in image 
+% If png must read and show as [img, cmap]
 img = imread('dennis.jpg');
-decLevels = 1;
-quantLevels = 20;
-wavename = 'haar';
-%[c,s] = wavedec2(img,2,'sym4');
+
+% Declares the two input vaiables. The Quantization step size and 
+% the chosen Wavelet
+quantLevels = str2double(input("Please Enter Amount Quantization Levels (1-20): ",'s'));
+wavename = input("Enter Desired Wavlet(haar, dbN, biorN.M, symN, coifN): ",'s');
+
+
+% Wavelet Deompositon
 [cA,cH,cV,cD] = dwt2(img,wavename);
 
+% Outputs a plot of the subbands for the decopmosed image
 figure;
 subplot(2,2,1);
-imshow(cA);
+imshow(uint8(cA));
 axis off
 title('cA')
  
@@ -28,15 +37,10 @@ imshow(cD);
 axis off
 title('cD')
 
-
-thresh = multithresh(img,quantLevels);
-value = [0 thresh(2:end) 255]; 
-seg_img = imquantize(img, thresh, value);
-
-%threshCMap = multithresh(cmap, quantLevels);
-%valueCMap = [0 threshCMap(2:end) 255]; 
-%seg_cmap = imquantize(cmap, thresh, valueCMap);
-
+% Quantization of each subband
+% threshXX are step values for the band
+% valueXX are the reconstruction values for the band
+% seg_XX is the quantized band
 threshCA = multithresh(cA,quantLevels);
 valueCA = [min(min(min(cA))) threshCA(2:end) max(max(max(cA)))];
 seg_CA = imquantize(cA,threshCA, valueCA);
@@ -53,14 +57,10 @@ threshCD = multithresh(cD,quantLevels);
 valueCD = [min(min(min(cD))) threshCD(2:end) max(max(max(cD)))];
 seg_CD = imquantize(cD,threshCD, valueCD);
 	 
-figure;
-imshow(seg_img);
-axis off
-title('RGB Segmented Image')
-
+% Outputs a plot showing the quantized subbands
 figure;
 subplot(2,2,1);
-imshow(seg_CA);
+imshow(uint8(seg_CA));
 axis off
 title('RGB Segmented cA')
  
@@ -79,22 +79,19 @@ imshow(seg_CD);
 axis off
 title('RGB Segmented cD')
 
-X = idwt2(seg_CA,seg_CH,seg_CV,seg_CD,'sym4');
-Y = idwt2(cA,cH,cV,cD,'sym4');
-%seg_I = waverec2(c,s,'sym4');
-%max(max(abs(double(img)-seg_I)))
+% Subband Reconstuction
+X = idwt2(seg_CA,seg_CH,seg_CV,seg_CD,wavename);
+
+% Outputs plot of original and final images
 figure;
-subplot(1,3,1);
+subplot(1,2,1);
 imshow(img);
 axis off
 title('Original Image')
-subplot(1,3,2);
+subplot(1,2,2);
 imshow(uint8(X));
 axis off
 title('Final Image')
-subplot(1,3,3);
-imshow(uint8(Y));
-axis off
-title('Non-Quantized Image')
-%title('2 levels');
 
+peaksnr = psnr(uint8(X),img);
+disp("Peak signal-to-noise ratio: " + peaksnr);
